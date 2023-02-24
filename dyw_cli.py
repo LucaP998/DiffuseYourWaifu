5b06e173897e995d42bdba20ba202b618ae78ce9
Trafitto <borchimarco@gmail.com>
2023-02-24 18:25:02 +0100

    add kanji name

--- dyw_cli.py
+++ dyw_cli.py
@@ -5,8 +5,7 @@ from src.diffuse_your_waifu import diffuse_your_waifu_client
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path" ,help="Path to a WAIFU", default=None)
-    #BUG: if we write argument that start with #
-    parser.add_argument("-e", "--extra", nargs="+", help="Add a list of extra hashtag", default=[])
+    parser.add_argument("-e", "--extra", nargs="+", help="Add a list of extra hashtag without the #", default=[])
     args = parser.parse_args()
     extra_hashtags = diffuse_your_waifu_client.correct_hashtags(args.extra)
     diffuse_your_waifu_client.upload_photo(args.path, extra_hashtags)
